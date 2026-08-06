4055c78efffbb1a2612cd71fd5341676273460f6
Milos Popovic <milos.pop.004@gmail.com>
2026-08-06 21:58:38 +0200

    implement jwt auth

---
+++ src/Security/UserChecker.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Security;
+
+use App\Entity\User;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
+use Symfony\Component\Security\Core\User\UserCheckerInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class UserChecker implements UserCheckerInterface
+{
+    public function checkPreAuth(UserInterface $user): void
+    {
+        if (!$user instanceof User) {
+            return;
+        }
+
+        if (!$user->isActivated()) {
+            throw new CustomUserMessageAuthenticationException('You need to activate your account before you log in.');
+        }
+    }
+
+    public function checkPostAuth(UserInterface $user): void {}
+}
